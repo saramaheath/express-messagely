04706840b50a294288964291832835d998951c5d
Sara Heath <saramaheath@gmail.com>
2022-07-04 14:34:05 -0500

    added registeruser and authenticate method to user model

--- models/user.js
+++ models/user.js
@@ -2,30 +2,69 @@
 
 /** User of the site. */
 
-class User {
+const { NotFoundError } = require("../expressError");
+const db = require("../db");
+const bcrypt = require("bcrypt");
 
+class User {
   /** Register new user. Returns
    *    {username, password, first_name, last_name, phone}
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
+	const result = await db.query(
+      `INSERT INTO users (username,
+							password, 
+							first_name, 
+							last_name,
+							phone,
+							join_at)
+				VALUES ($1, $2, $3, $4, $5, current_timestamp)
+				RETURNING username, password, first_name, last_name, phone)`,
+      [username, password, first_name, last_name, phone]
+    );
+
+	const user = result.rows[0];
+	return new User(user);
   }
 
   /** Authenticate: is username/password valid? Returns boolean. */
 
   static async authenticate(username, password) {
+	const result = await db.query(
+		`SELECT username, password
+			FROM users
+			WHERE username = $1 AND password = $2`,
+			[username, password]
+	);
+	const user = result.rows[0];
+	if (!user){
+		return false;
+	}
+	return true;
   }
 
-  /** Update last_login_at for user */
-
+  /** Update last_login_at for user
+   * TODO: current timestamp to add timezone
+   */
   static async updateLoginTimestamp(username) {
+	const result = await db.query(
+		`UPDATE users
+			SET last_login_at = current_timestamp
+			WHERE username = $1
+			RETURNING last_login_at`,
+			[username]
+	);
+
+	const user = result.rows[0];
+	if (!user) throw new NotFoundError(`No such user: ${username}`);
+	return user.last_login_at; 
   }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name}, ...] */
 
-  static async all() {
-  }
+  static async all() {}
 
   /** Get: get user by username
    *
@@ -36,8 +75,7 @@ class User {
    *          join_at,
    *          last_login_at } */
 
-  static async get(username) {
-  }
+  static async get(username) {}
 
   /** Return messages from this user.
    *
@@ -47,8 +85,7 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesFrom(username) {
-  }
+  static async messagesFrom(username) {}
 
   /** Return messages to this user.
    *
@@ -58,9 +95,7 @@ class User {
    *   {id, first_name, last_name, phone}
    */
 
-  static async messagesTo(username) {
-  }
+  static async messagesTo(username) {}
 }
-
 
 module.exports = User;
